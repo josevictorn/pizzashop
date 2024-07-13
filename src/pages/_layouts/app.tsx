cea9f034b07447e0793b8100673f814616778f39
victor <josevictornascimento2016@gmail.com>
2024-07-13 18:11:48 -0300

    :sparkles: feat: create sign-in page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,13 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AppLayout() {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between b">
+        <Pizza className="h-5 w-5" />
+        <span className="font-semibold">pizza.shop</span>
+      </div>
 
       <div>
         <Outlet />
