14528952f9510dc63599c889a113948ca4f3df64
victor <josevictornascimento2016@gmail.com>
2024-07-13 18:29:54 -0300

    :sparkles: feat: configure toast notification with Sonner

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { router } from "./pages/routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import './global.css'
+import { Toaster } from "sonner";
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
