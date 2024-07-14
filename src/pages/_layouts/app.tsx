05285061085d5256bb225d583ed513825c8c1e30
victor <josevictornascimento2016@gmail.com>
2024-07-13 22:20:09 -0300

    :sparkles: feat: create header of app

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,15 +1,14 @@
-import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
+
+import { Header } from '@/components/header'
 
 export function AppLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2">
+    <div className="grid min-h-screen grid-cols-2 antialiased">
       <div className="flex h-full flex-col justify-between b">
-        <Pizza className="h-5 w-5" />
-        <span className="font-semibold">pizza.shop</span>
+        <Header />
       </div>
-
-      <div>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
