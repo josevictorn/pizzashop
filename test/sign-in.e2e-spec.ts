89e31c57e459c5efea76dea709915d44a23d2d0e
José Victor <josevictornascimento2016@gmail.com>
2024-08-04 16:05:58 -0300

    fix: Async Assertions

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -10,9 +10,7 @@ test('sign in successfully', async ({ page }) => {
     'Enviamos um link de autenticação para o seu e-mail.',
   )
 
-  expect(toast).toBeVisible()
-
-  await page.waitForTimeout(2000)
+  await expect(toast).toBeVisible()
 })
 
 test('sign in with wrong credentials', async ({ page }) => {
@@ -23,7 +21,7 @@ test('sign in with wrong credentials', async ({ page }) => {
 
   const toast = page.getByText('Credenciais inválidas.')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to new restaurante page', async ({ page }) => {
--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -20,11 +20,11 @@ test('update profile successfully', async ({ page }) => {
 
   const toast = page.getByText('Perfil atualizado com sucesso!')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 
   await page.getByRole('button', { name: 'Close' }).click()
 
-  expect(
+  await expect(
     page.getByRole('button', { name: 'Rocket Pizza' }).first(),
   ).toBeVisible()
 })
